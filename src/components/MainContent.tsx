006aba08c61ed4ce07d82529e0cd0a705828386a
= <=>
2025-04-26 20:53:54 -0300

    feat: enhance MainContent component with product filtering and API integration

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { Tally3 } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useFilter } from "./FilterContext";
+import axios from "axios";
 
 const MainContent = () => {
-  return (
-    <div>MainContent</div>
-  )
-}
+  const { searchQuery, selectedCategory, minPrice, maxPrice, keyword } =
+    useFilter();
 
-export default MainContent
+  const [products, setProducts] = useState<any[]>([]);
+  const [filter, setFilter] = useState("all");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const itemsPerPage = 12;
+
+  useEffect(() => {
+    let url = `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${(currentPage - 1) * itemsPerPage}`;
+
+    if (keyword) {
+        url += `&q=${keyword}`;
+    }
+
+    axios.get(url)
+        .then((response) => {
+            setProducts(response.data.products);
+            console.log('Productos: ', response.data.products);
+        })
+        .catch((error) => {
+            console.error('Error fetching products:', error);
+        })
+  }, [currentPage, keyword])
+
+  return (
+    <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
+      <div className="mb-5">
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          <div className="relative my-5">
+            <button className="border px-4 py-2 rounded-full flex items-center">
+              <Tally3 className="mr-2" />
+              {filter === "all"
+                ? "Filter"
+                : filter.charAt(0).toLowerCase() + filter.slice(1)}
+            </button>
+
+            {dropdownOpen && (
+                <div className="absolute bg-white border border-purple-300 rounded mt-2 w-full sm:w-40">
+                    <button 
+                        onClick={() => setFilter('cheap')} 
+                        className="block px-4 py-2 w-full text-left bg-purple-900 hover:bg-purple-600 hover:text-purple-100">
+                        Cheap
+                    </button>
+                    <button 
+                        onClick={() => setFilter('expensive')} 
+                        className="block px-4 py-2 w-full text-left bg-purple-900 hover:bg-purple-600 hover:text-purple-100">
+                        Expensive
+                    </button>
+                    <button 
+                        onClick={() => setFilter('popular')} 
+                        className="block px-4 py-2 w-full text-left bg-purple-900 hover:bg-purple-600 hover:text-purple-100">
+                        Popular
+                    </button>
+                </div>
+            )}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-5">
+            {/* BookCard */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainContent;
