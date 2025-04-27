34439745c8f54588bc77d35ac82db177af650c75
= <=>
2025-04-27 18:21:24 -0300

    feat: implement ProductPage component with product data fetching and loading state

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+interface ProductProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    rating: number;
+    images: string[];
+}
 
 const ProductPage = () => {
+    const { id } = useParams<{ id: string }>();
+    const navigate = useNavigate();
+    const [product, setProduct] = useState<ProductProps | null>(null);
+
+    useEffect(() => {
+        if (id) {
+            axios
+                .get<ProductProps>(`https://dummyjson.com/products/${id}`)
+                .then((response) => {
+                    setProduct(response.data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching product data:", error);
+                });
+        }
+    }, [id]);
+
+    if (!product) {
+        return <div>Loading...</div>;
+    }
+
   return (
-    <div>ProductPage</div>
+    <div>
+
+    </div>
   )
 }
 
